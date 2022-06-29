628c94326bf4e7a7eb3c36e8d385437f67a503c6
RadimaS <selimova248@gmail.com>
2022-06-30 00:18:46 +0300

    create ws

---
+++ js/main.js
@@ -0,0 +1,58 @@
+var swiper = new Swiper(".categories-slider", {
+  loop: false,
+  clickable: true,
+  
+  navigation: {
+    nextEl: '.categories__button--prev',
+    prevEl: '.categories__button--next',
+  },
+});
+
+var swiper = new Swiper(".unpublished-slider", {
+  loop: false,
+  clickable: true,
+  
+  navigation: {
+    nextEl: '.unpublished__button--next',
+    prevEl: '.unpublished__button--prev',
+  },
+
+  breakpoints: {
+    1920: {
+      spaceBetween: 30,
+    },
+    1440: {
+      spaceBetween: 30,
+      slidesPerView: 5,
+    },
+    1200: {
+        spaceBetween: 10,
+        slidesPerView: 5,
+    },
+    992: {
+      spaceBetween: 10,
+      slidesPerView: 4,
+    },
+    767: {
+      spaceBetween: 10,
+      slidesPerView: 3,
+    },
+    576: {
+      spaceBetween: 10,
+      slidesPerView: 3,
+    },
+    480: {
+      spaceBetween: 10,
+      slidesPerView: 1,
+    },
+  },
+});
+    
+
+
+// var menuButton = document.querySelector(".navbar-button")
+// menuButton.addEventListener('click', function () {
+//   console.log('Клик по кнопке меню')
+//   document.querySelector(".navbar__nav")
+//   .classList.toggle('navbar__nav--visible');
+// })
